38059ac81f89141680ca38c359469f6e90c3fa45
Joao-Vitoor <84918323+Joao-Vitoor@users.noreply.github.com>
2022-09-28 08:26:08 -0300

    Add files via upload

---
+++ vitor/js/carrosel.js
@@ -0,0 +1,13 @@
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $('.apresentacao').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 1000,
+  });
